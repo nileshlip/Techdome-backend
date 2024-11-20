8c43274b482e3cc9affa4323e3d90a183a9f79c6
nileshlip <135098516+nileshlip@users.noreply.github.com>
2024-11-20 19:44:26 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+const uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
